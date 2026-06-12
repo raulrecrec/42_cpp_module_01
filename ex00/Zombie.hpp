a4457eb6700be91333809087d818d6341197a782
WSL HOME <raulrecrec@gmail.com>
2026-06-12 19:12:22 +0200

    Update Zombie header declarations
    - Added prototypes for newZombie and randomChump.
    - Declared helper functions outside the Zombie class.
    - Prepared the header to expose all required ex00 functions.

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/12 17:23:37 by rexposit          #+#    #+#             */
-/*   Updated: 2026/06/12 18:03:40 by rexposit         ###   ########.fr       */
+/*   Updated: 2026/06/12 19:09:01 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,8 @@ class Zombie
 	void	announce(void) const;
 	void	set_name(std::string zombie_name);
 };
+
+Zombie	*newZombie(std::string zombie_name);
+void	randomChump(std::string zombie_name);
 
 #endif
