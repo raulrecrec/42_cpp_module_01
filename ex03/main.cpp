2cc16a4c2c0914b6d022f2b31aeb3f8a149da7e6
rexposit <raulrecrec@gmail.com>
2026-06-13 13:31:00 +0200

    Add weapon behavior tests
    - Created main.cpp to test HumanA and HumanB behavior.
    - Verified that weapon type changes are reflected during attacks.
    - Added tests based on the exercise subject examples.

---
+++ ex03/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/13 12:24:39 by rexposit          #+#    #+#             */
+/*   Updated: 2026/06/13 13:29:18 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+
+int main()
+{
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+	return (0);
+}
