c2aa750a5d97491c7b94efa781be75cdb7b8cb87
WSL HOME <raulrecrec@gmail.com>
2026-06-16 11:20:53 +0200

    Add Harl complaint tests
    - Created main.cpp to test all Harl complaint levels.
    - Called complain with DEBUG, INFO, WARNING and ERROR.
    - Added an invalid level test to verify fallback behavior.

---
+++ ex05/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/16 10:17:45 by rexposit          #+#    #+#             */
+/*   Updated: 2026/06/16 11:20:07 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+#include <iostream>
+
+int	main(void)
+{
+	Harl	harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("INVALID");
+
+	return (0);
+}
