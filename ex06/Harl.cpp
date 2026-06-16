da9e2cc3f0bb68df9914d08cb65c6c886ed4d5fd
WSL HOME <raulrecrec@gmail.com>
2026-06-16 11:43:06 +0200

    Add Harl filter complaint system
    - Created Harl class files for the filter exercise.
    - Implemented complaint messages with level headers.
    - Added fallback output for insignificant problems.

---
+++ ex06/Harl.cpp
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/16 10:17:39 by rexposit          #+#    #+#             */
+/*   Updated: 2026/06/16 11:40:45 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+#include <iostream>
+
+void	Harl::debug(void)
+{
+	std::cout << "[ DEBUG ]\n";
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n";
+}
+
+void	Harl::info(void)
+{
+	std::cout << "[ INFO ]\n";
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\n";
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "[ WARNING ]\n";
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month.\n";
+}
+
+void	Harl::error(void)
+{
+	std::cout << "[ ERROR ]\n";
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n";
+}
+
+void	Harl::complain(std::string level)
+{
+	void	(Harl::*funcs[4])(void);
+
+	funcs[0] = &Harl::debug;
+	funcs[1] = &Harl::info;
+	funcs[2] = &Harl::warning;
+	funcs[3] = &Harl::error;
+
+	std::string	levels[4];
+
+	levels[0] = "DEBUG";
+	levels[1] = "INFO";
+	levels[2] = "WARNING";
+	levels[3] = "ERROR";
+
+	int	i = 0;
+	while (i < 4)
+	{
+		if (level == levels[i])
+		{
+			(this->*funcs[i])();
+			return ;
+		}
+		i++;
+	}
+	std::cout << "[ Probably complaining about insignificant problems ]\n";
+}
